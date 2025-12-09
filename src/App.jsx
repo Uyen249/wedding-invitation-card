9f7b28336c7a21a11c10e2f56fa9b4c38d07412c
Uyen249 <uyenpanda2k1@gmail.com>
2025-12-09 22:26:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
   const headerPhotos = [
     'https://i.ibb.co/zVL0qRP6/DSC02972.jpg',
     'https://i.ibb.co/pvfMSxTc/DSC03536.jpg',
-    'https://i.ibb.co/Fk73F03W/DSC03816.jpg'
+    'https://i.ibb.co/jkSLJwHH/DSC03816.jpg'
   ];
 
   const weddingDate = new Date('2025-12-28T11:00:00');
@@ -822,6 +822,7 @@ export default function App() {
     </div>
   );
 }
+
 
 
 
