c6891b7597499c5099c88a23d5eea6e4c49c2618
Uyen249 <uyenpanda2k1@gmail.com>
2025-12-12 16:50:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const goToPrev = () => {
     'https://raw.githubusercontent.com/Uyen249/wedding-invitation-card/main/public/DSC03884.jpg',
     'https://raw.githubusercontent.com/Uyen249/wedding-invitation-card/main/public/DSC03896.jpg',
     'https://raw.githubusercontent.com/Uyen249/wedding-invitation-card/main/public/DSC03907.jpg',
-    'https://raw.githubusercontent.com/Uyen249/wedding-invitation-card/main/public/DSC03938.jpg',
+    'https://raw.githubusercontent.com/Uyen249/wedding-invitation-card/main/public/DSC03938.jpg'
 ];
 
   const calendarDays = () => {
@@ -896,6 +896,7 @@ const goToPrev = () => {
     </div>
   );
 }
+
 
 
 
